e00a6f9fdc348e25cf59016438b67c2d76505a66
terminalcmd <170613258+terminalcmd@user.noreply.github.com>
2024-06-09 12:23:07 +0530

    Modified index.js Remove some unnecessry comments

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-//Keeping the score from user and computer 
+
 let humanScore = 0
 let computerScore = 0
 
-//Computer logic 
 //It return random choices from 'rock,paper,scissors'
 
-
-//Creating a function for computer choice
 function getComputerChoice(){
     let choices ="rock,paper,scissors"
-//Changing choices into array 
     let newchoice = choices.split(',')
-//Generate a random choice
     let random = Math.floor((Math.random()*newchoice.length))
     let randomChoice = newchoice[random]
     return randomChoice
 }
 
 
-//Creating logic for human choice
 //It return user input from the given choices 
 
 function getHumanChoice(){
@@ -32,16 +26,6 @@ function getHumanChoice(){
 }
 
 //Logic to play one round 
-//To take user and computer choices
-//Check who the winner get increase the score counter
-//And prints the winner
-
-
-///Pseudo code
-
-///taking user and computer value 
-///if user is rock and comp is scissors user wins score+1
-///if comp is rock and user is scissors comp wins score+1
 
 function playRound(humanChoice,computerChoice){
     let messageWin=`You win ${humanChoice} beats ${computerChoice}`
